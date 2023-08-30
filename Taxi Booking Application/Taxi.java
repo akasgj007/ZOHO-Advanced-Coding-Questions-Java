a6e1b5d4d72101d8e36e5ab0c69c40d7991e05b3
Akash Babu GJ <36225867+akasgj007@users.noreply.github.com>
2023-08-30 19:58:15 +0530

    Update Taxi.java

--- Taxi Booking Application/Taxi.java
+++ Taxi Booking Application/Taxi.java
@@ -1,46 +1,72 @@
 import java.util.*;
 
 public class Taxi {
-    static int taxiCount = 0;
-    int id;
-    boolean booked;
-    char currentSpot;
-    int freeTime;
-    int totalEarnings;
-    List<String> trips; // details of all trips of this taxi
 
-    public Taxi() {
-        booked = false;
-        currentSpot = 'A';
-        freeTime = 6;
-        totalEarnings = 0;
-        trips = new ArrayList<>();
-        taxiCount += 1;
-        id = taxiCount;
+  static int taxiCount = 0;
+  int id;
+  boolean booked;
+  char currentSpot;
+  int freeTime;
+  int totalEarnings;
+  List<String> trips; // details of all trips of this taxi
+
+  public Taxi() {
+    booked = false;
+    currentSpot = 'A';
+    freeTime = 6;
+    totalEarnings = 0;
+    trips = new ArrayList<>();
+    taxiCount += 1;
+    id = taxiCount;
+  }
+
+  public void setDetails(
+    boolean booked,
+    char currentSpot,
+    int freeTime,
+    int totalEarnings,
+    String tripDetail
+  ) {
+    this.booked = booked;
+    this.currentSpot = currentSpot;
+    this.freeTime = freeTime;
+    this.totalEarnings = totalEarnings;
+    this.trips.add(tripDetail);
+  }
+
+  public void printDetails() {
+    System.out.println(
+      "Taxi - " + this.id + " | Total Earnings - " + this.totalEarnings + "\n"
+    );
+    System.out.println(
+      "TaxiID      BookingID       CustomerID      From        To      PickupTime      DropTime        Amount"
+    );
+    System.out.println(
+      "----------------------------------------------------------------------------------------------------------------"
+    );
+    if (trips.size() == 0) {
+      System.out.println("No data found...");
+    } else {
+      for (String trip : trips) {
+        System.out.println(id + "           " + trip);
+      }
     }
 
-    public void setDetails(boolean booked, char currentSpot, int freeTime, int totalEarnings, String tripDetail) {
-        this.booked = booked;
-        this.currentSpot = currentSpot;
-        this.freeTime = freeTime;
-        this.totalEarnings = totalEarnings;
-        this.trips.add(tripDetail);
-    }
+    System.out.println(
+      "----------------------------------------------------------------------------------------------------------------\n\n"
+    );
+  }
 
-    public void printDetails() {
-        System.out.println("Taxi - " + this.id + " Total Earnings - " + this.totalEarnings);
-        System.out.println(
-                "TaxiID      BookingID       CustomerID      From        To      PickupTime      DropTime        Amount");
-        for (String trip : trips) {
-            System.out.println(id + "        " + trip);
-        }
-        System.out.println(
-                "----------------------------------------------------------------------------------------------------------------");
-    }
-
-    public void printTaxiDetails() {
-        System.out.println("Taxi - " + this.id + " Total Earnings - " + this.totalEarnings + " Current Spot - "
-                + this.currentSpot + " Free Time - " + this.freeTime);
-    }
-
+  public void printTaxiDetails() {
+    System.out.println(
+      "Taxi - " +
+      this.id +
+      " | Total Earnings - " +
+      this.totalEarnings +
+      " | Current Spot - " +
+      this.currentSpot +
+      " | Free Time - " +
+      this.freeTime
+    );
+  }
 }
